1103ac132c0b44490d074c6d14fedb97a736c0c3
TonuFish <Arkaicore@outlook.com>
2021-06-04 21:59:55 +0800

    Add project files.

---
+++ samples/EnumScribe.Samples/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using EnumScribe;
+
+namespace EnumScribe.Samples
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ tests/EnumScribe.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using EnumScribe;
+using FluentAssertions;
+using Xunit;
+
+namespace EnumScribe.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
