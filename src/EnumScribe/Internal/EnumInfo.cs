8254a2631c85b946359fca78de91430e1ccd2df1
TonuFish <Arkaicore@outlook.com>
2022-01-13 23:14:12 +0800

    Replace enum file manual syntax generation with a syntax factory

--- src/EnumScribe/Internal/EnumInfo.cs
+++ src/EnumScribe/Internal/EnumInfo.cs
@@ -4,7 +4,13 @@ namespace EnumScribe.Internal
 {
     internal sealed class EnumInfo
     {
+        /// <summary>
+        /// Fully qualified name.
+        /// </summary>
         public string FullName { get; set; } = null!;
+        public bool InGlobalNamespace { get; set; }
+        public string OutputFullName => InGlobalNamespace ? $"global::{FullName}" : FullName;
+
         public List<(string Name, string Description)> EnumNameDescriptionPairs { get; set; } = null!;
     }
 }
