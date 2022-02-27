af2b616428ed3c4285e66d4f4778b9cffa81e4f9
TonuFish <Arkaicore@outlook.com>
2022-02-27 16:35:28 +0800

    Scaffold test project, using xUnit + Verify now. Implement a dummy test

---
+++ tests/EnumScribe.Tests/Initialisation.cs
@@ -0,0 +1,25 @@
+﻿using VerifyTests;
+using System.Runtime.CompilerServices;
+
+#if !NET5_0_OR_GREATER
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public sealed class ModuleInitializerAttribute : Attribute { }
+}
+#endif
+
+namespace EnumScribe.Tests
+{
+    public static class ModuleInitializer
+    {
+        /// <summary>
+        /// Called on assembly load, some initialisation required by Verify.SourceGenerators.
+        /// </summary>
+        [ModuleInitializer]
+        public static void Init()
+        {
+            VerifySourceGenerators.Enable();
+        }
+    }
+}
